4d1f14e4d5386f753d43ee7f55e946d4b2dd0c3b
github username <github email address>
2018-07-23 16:00:47 -0500

    pokemon scrapper sql execute pairing mod1

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(name)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute('INSERT INTO pokemon (name, type) VALUES (?, ?)', [name, type])
+  end
+
+  def self.find(id, db)
+    var = db.execute('SELECT * FROM pokemon WHERE (?)', [id])
+    new = Pokemon.new(var[0][1])
+    new.type = var[0][2]
+    new.id = var[0][0]
+    new
+  end
 end
